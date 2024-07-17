6cf0458625ba53511f68086d9d8afe3883af0dde
${GITHUB_USER} <${GITHUB_MAIL}>
2024-07-17 01:05:05 -0300

    update

--- datawarehouse/models/datamart/dm_tickers.sql
+++ datawarehouse/models/datamart/dm_tickers.sql
@@ -1,7 +1,7 @@
 WITH ticker_data AS (
     SELECT
         ticker,
-        ROUND(AVG(valor_fechamento)::numeric, 2) AS media_valor_fechamento_USD
+        ROUND(AVG(valor_fechamento)::numeric, 2) AS media_fechamento_USD
     FROM
         {{ ref('stg_tickers') }}
     GROUP BY
